38cd70969ddad810b71dd1a871d82ca587909408
Ramdeniakki <ramdeniakki35@gmail.com>
2025-02-01 20:14:42 +0530

    Added Signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,37 +1,56 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-
 import { JWT_SECERT } from "@repo/backend-common/config";
 import { CreateUserSchema } from "@repo/common/types";
+import { prisma } from "@repo/db/client";
+import express from "express";
+import jwt from "jsonwebtoken";
 const app = express();
-
-app.post("/signup",(req,res)=>{
-
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
-        res.json({
-            message:"Incorrect details"
-        })
-        return;
-    }
-    //dbcall
-    res.status(200).json({
-        userId:"123"
-    })
-    
-})
-app.post("/signin",(req,res)=>{
-    const userId = 1;
-    const token = jwt.sign({
-        userId,
-    },JWT_SECERT)
-
+app.post("/signup", async (req, res) => {
+  const prasedData =   CreateUserSchema.safeParse(req.body);
+  if (!prasedData.success) {
     res.json({
-        token
-    })
+      message: "Incorrect details",
+    });
+    return;
     
-})
-app.post("/room",(req,res)=>{
-    
-})
-app.listen(3003);   
+  }
+ try{
+
+    await prisma.user.create({
+        data: {
+          email: prasedData.data?.username,
+          password: prasedData.data.password,
+          name: prasedData.data.name,
+        },
+      });
+
+      res.json({
+        userId:"123"
+      })
+
+ }
+ catch(err){
+   res.status(403).json({
+    message:"User is Already  exist  this username"
+   })
+ }
+   
+  //dbcall
+  res.status(200).json({
+    userId: "123",
+  });
+});
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECERT
+  );
+
+  res.json({
+    token,
+  });
+});
+app.post("/room", (req, res) => {});
+app.listen(3003);
